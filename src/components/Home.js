69701154b1db0826aaee41a4fab2a0001dd41c51
Drenovaa <guilherme_silva_123@hotmail.com>
2021-05-12 15:34:46 -0300

    criação de itens

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import styled from 'styled-components'
 
 
+class Home extends React.Component {
 
+    render() {
+        return (
+            <div>
+                <h1>HOME</h1>
+            </div>
+        )
+    }
+}
 
-export default Default;
+export default Home
