c208be8bddf1b72522e4cc2893051e98f46360c8
Drenovaa <guilherme_silva_123@hotmail.com>
2021-05-12 17:13:09 -0300

    produtos render

--- src/App.js
+++ src/App.js
@@ -3,35 +3,36 @@ import styled from 'styled-components'
 import './App.css'
 import Home from "./components/Home"
 
+const products = [
+  {
+    id: Math.random(),
+    nome: "Uniforme Cyberpunk",
+    preço: 8600,
+    imagem: "https://mega.ibxk.com.br/2015/03/13/13142708050663.jpg"
+    
+  },
+  {
+    id: Math.random(),
+    nome: "Confort 101",
+    preço: 9000,
+    imagem: "https://www.inovacaotecnologica.com.br/noticias/imagens/010130190521-roupa-espacial-para-marte-1.jpg"
+  },
+  {
+    id: Math.random(),
+    nome: "OITNB",
+    preço: 7300,
+    imagem: "https://i.pinimg.com/736x/d9/45/21/d94521ee32233b8ad3a3befe7d85242a.jpg"
+  },
+  {
+    id: Math.random(),
+    nome: "Safety first",
+    preço: 5600,
+    imagem: "https://w7.pngwing.com/pngs/205/871/png-transparent-person-wearing-astronaut-attire-astronaut-space-suit-extravehicular-activity-outer-space-health-astronaut-disease-space-weightlessness.png"
+  }     
+]
 
 class App extends React.Component {
     state = {
-      products:[
-        {
-          id: Math.random(),
-          nome: "Uniforme Cyberpunk",
-          preço: 8600,
-          imagem: "https://mega.ibxk.com.br/2015/03/13/13142708050663.jpg"
-        },
-        {
-          id: Math.random(),
-          nome: "Confort 101",
-          preço: 9000,
-          imagem: "https://www.inovacaotecnologica.com.br/noticias/imagens/010130190521-roupa-espacial-para-marte-1.jpg"
-        },
-        {
-          id: Math.random(),
-          nome: "OITNB",
-          preço: 7300,
-          imagem: "https://i.pinimg.com/736x/d9/45/21/d94521ee32233b8ad3a3befe7d85242a.jpg"
-        },
-        {
-          id: Math.random(),
-          nome: "Safety first",
-          preço: 5600,
-          imagem: "https://w7.pngwing.com/pngs/205/871/png-transparent-person-wearing-astronaut-attire-astronaut-space-suit-extravehicular-activity-outer-space-health-astronaut-disease-space-weightlessness.png"
-        }     
-      ],
       myCart:[],
       lowerFilter: 0,
       higherFilter: Infinity,
@@ -56,7 +57,10 @@ class App extends React.Component {
     render() {  
       return(
         <div>
-          <Home/>
+          <Home
+          products={products}
+          addToCart={this.addToCart}
+          />
         </div>
       )
       
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import styled from 'styled-components'
+import { Product } from './Product';
+
 
 
 class Home extends React.Component {
 
     render() {
-        return (
-            <div>
-                <h1>HOME</h1>
+        const productList = this.props.products
+        return(<div>
+            {productList.map((product) => {
+                return <Product
+                  product={product}
+                  onAddProductToCart={this.props.onAddProductToCart}
+                />
+              })}
             </div>
         )
+
     }
 }
 
